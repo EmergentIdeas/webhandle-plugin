73c91812dae407aed66ac6a6cac6ceca133e7534
Dan Kolz <dan@emergentideas.com>
2013-10-02 14:18:07 -0500

    added quick way to change all of the organization and module names

---
+++ personalize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "The organization name: " orgName
+read -p "The project name: " projectName
+read -p "The root package path: " packagePath
+sed -i "s/change-me-to-organization-name/$orgName/g" build.xml
+sed -i "s/change-me-to-project-name/$projectName/g" build.xml
+
+sed -i "s,change/me/to/your/project/package/name,$packagePath,g" build.xml
+
+sed -i "s/change-me-to-organization-name/$orgName/g" ivy.xml
+sed -i "s/change-me-to-project-name/$projectName/g" ivy.xml
+
+echo "build.xml and ivy.xml changed"
