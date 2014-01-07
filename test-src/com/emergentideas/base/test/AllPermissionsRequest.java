8923d6c9d743b3439f8353c399fa6eea254616c1
Dan Kolz <dan@emergentideas.com>
2014-01-07 11:07:26 -0600

    request no identifies user prinicpal as administrator

--- test-src/com/emergentideas/base/test/AllPermissionsRequest.java
+++ test-src/com/emergentideas/base/test/AllPermissionsRequest.java
@@ -1,5 +1,7 @@
 package com.emergentideas.base.test;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
@@ -8,6 +10,19 @@ public class AllPermissionsRequest extends HttpServletRequestWrapper {
 	public AllPermissionsRequest(HttpServletRequest request) {
 		super(request);
 	}
+
+	
+	@Override
+	public Principal getUserPrincipal() {
+		return new Principal() {
+			
+			@Override
+			public String getName() {
+				return "administrator";
+			}
+		};
+	}
+
 
 	@Override
 	public boolean isUserInRole(String role) {
